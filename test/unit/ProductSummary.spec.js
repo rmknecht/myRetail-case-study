2a12efd2004b9732cab0f98e936274168681f91d
Rob Knecht <rmknecht@gmail.com>
2017-12-28 02:41:38 -0600

    Add a test for the product summary component.

--- test/unit/ProductSummary.spec.js
+++ test/unit/ProductSummary.spec.js
@@ -11,4 +11,20 @@ describe('ProductSummary Component', () => {
       store
     });
   });
+
+  // Inspect the raw component options
+  it('Increments and decrements product quanity by one', () => {
+  	const inc = wrapper.find('.quantity-inc');
+  	const dec = wrapper.find('.quantity-dec');
+
+  	inc.trigger('click');
+
+  	const label = wrapper.find('span.product-quantity-value');
+
+    expect(label.text()).toBe('2');
+
+    dec.trigger('click');
+
+    expect(label.text()).toBe('1');
+  });
 });
